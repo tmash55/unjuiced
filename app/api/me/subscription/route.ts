5387e6982485b9804fb27b63a72b2c525fb1b5cb
Tyler Maschoff <Tyler.Maschoff@fbfs.com>
2025-10-14 09:20:31 -0500

    fix sub route error

--- app/api/me/subscription/route.ts
+++ app/api/me/subscription/route.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/libs/supabase/server";
 
 export const runtime = 'nodejs'
